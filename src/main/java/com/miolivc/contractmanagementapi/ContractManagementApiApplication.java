5c1d8836190320cc59d7ee73d0d67dd03ddd1c64
Michelle Oliveira <michelleolivcosta@gmail.com>
2021-08-26 14:34:20 -0300

    Setting up project

---
+++ src/main/java/com/miolivc/contractmanagementapi/ContractManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.miolivc.contractmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miolivc/contractmanagementapi/ContractManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miolivc.contractmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
